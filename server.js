8cfc3380c5e2d058c713cc4a56fa07253b2eee07
Everett Smith <evepsmith15@gmail.com>
2019-07-26 22:48:55 -0700

    added gitignore

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ var path = require("path");
 // =============================================================
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    res.send('hello world')
-})
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
